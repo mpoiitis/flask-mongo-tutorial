b35acc00813f5b5734875fd858238c98a79b63f3
Marinos Poiitis <marinospoet@gmail.com>
2020-02-20 11:59:19 +0200

    Add whole application

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+from db import Db
+
+app = Flask(__name__, template_folder='templates')
+db = Db()
+
+
+@app.route("/")
+def home():
+    todos = db.find_all('todos')
+    return render_template('home.html', todos=list(todos))
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='127.0.0.1', port=5110)
