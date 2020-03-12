d462e4c55edbb99b6aec07077278ab4d4f2a57b7
mpoiitis <marinospoet@gmail.com>
2020-03-12 15:57:27 +0200

    Add my life

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import pymongo
 
 class Db:
     def __init__(self):
-        client = pymongo.MongoClient('localhost', 27017)
+        client = pymongo.MongoClient('mongodb+srv://dbUser:dbUserPassword@cluster0-lq1ei.mongodb.net/test?retryWrites=true&w=majority', 27017)
         self.db = client['todo-db']
 
     def find_all(self, collection_name: str):
