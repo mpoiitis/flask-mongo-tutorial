0c150d057e48c4e8881b34c1d1a68d3f28254630
mpoiitis <marinospoet@gmail.com>
2020-03-12 15:26:59 +0200

    Add my life

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template
 import os
-# from db import Db
+from db import Db
 
 app = Flask(__name__, template_folder='templates')
-# db = Db()
+db = Db()
 
-
-# @app.route("/")
-# def home():
-#     todos = db.find_all('todos')
-#     return render_template('home.html', todos=list(todos))
 
 @app.route("/")
-def hello():
-    return "Hello world!"
+def home():
+    todos = db.find_all('todos')
+    return render_template('home.html', todos=list(todos))
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
