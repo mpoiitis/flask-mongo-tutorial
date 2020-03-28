df060316820e23566ce4b629ac0108ea4cfc57ff
Marinos Poiitis <marinospoet@gmail.com>
2020-03-28 15:03:25 +0200

    Add latest update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import os
 from funcs.db import Db
 
@@ -11,6 +11,10 @@ def home():
     todos = db.find_all('todos')
     return render_template('home.html', todos=list(todos))
 
+@app.route("/todo/<importance_val>")
+def get_todo(importance_val):
+    todos = db.find('todos', {"importance": int(importance_val)})
+    return jsonify(todos)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- funcs/db.py
+++ funcs/db.py
@@ -3,8 +3,11 @@ import pymongo
 
 class Db:
     def __init__(self):
-        client = pymongo.MongoClient('mongodb+srv://dbUser:dbUserPassword@cluster0-lq1ei.mongodb.net/test?retryWrites=true&w=majority', 27017)
+        client = pymongo.MongoClient('localhost', 27017)
         self.db = client['todo-db']
 
     def find_all(self, collection_name: str):
         return list(self.db[collection_name].find({}, {'_id': False}))
+
+    def find(self, collection_name:str, query:dict):
+        return list(self.db[collection_name].find(query, {'_id': False}))
